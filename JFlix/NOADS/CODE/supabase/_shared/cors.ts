22140a73404dbd7a40f498e90384852c8168b4e0
Connected140601 <junrel.sapantaicloud@gmail.com>
2025-07-03 01:16:44 +0800

    Delete JFlix directory

--- JFlix/NOADS/CODE/supabase/_shared/cors.ts
+++
@@ -1,4 +0,0 @@
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*', // Or your specific frontend domain for production: e.g. 'https://yourdomain.com'
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-};
